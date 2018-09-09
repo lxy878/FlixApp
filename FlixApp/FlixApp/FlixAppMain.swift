7059b27ca2c0bdbd3de0b487dc93436de23bd616
lxy878 <lxy878@msn.cn>
2018-09-09 13:40:36 -0400

    creating a network request

--- FlixApp/FlixApp/FlixAppMain.swift
+++ FlixApp/FlixApp/FlixAppMain.swift
@@ -12,24 +12,31 @@ class FlixAppMain: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // create url and url request
+        // "!"-unswrap
+        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        // cachePolicy means what action url will do when network is connected.
+        // reloadIgnoringLocalCacheData means that always load data from network, even the data is in the local cache (only for testing)
+        let request =  URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        // get data
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error{
+                print(error.localizedDescription)
+            }else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+                print(dataDictionary)
+            }
+        }
+        // call task
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
