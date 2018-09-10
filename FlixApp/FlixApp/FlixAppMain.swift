2f357cc642c004baa5988f35bc86c41a34adea3a
lxy878 <lxy878@msn.cn>
2018-09-10 00:22:30 -0400

    TableView +Data

--- FlixApp/FlixApp/FlixAppMain.swift
+++ FlixApp/FlixApp/FlixAppMain.swift
@@ -8,13 +8,20 @@
 
 import UIKit
 
-class FlixAppMain: UIViewController {
-
+class FlixAppMain: UIViewController, UITableViewDataSource{
+    
+    @IBOutlet weak var tableView: UITableView!
+    var movieList : [[String : Any]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
+        
         // create url and url request
         // "!"-unswrap
-        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=   ")!
         // cachePolicy means what action url will do when network is connected.
         // reloadIgnoringLocalCacheData means that always load data from network, even the data is in the local cache (only for testing)
         let request =  URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -29,14 +36,29 @@ class FlixAppMain: UIViewController {
                 //print(dataDictionary)
                 // get data in dictionary of the "results" key
                 let movies = dataDictionary["results"] as! [[String:Any]]
-                for movie in movies{
-                    let title = movie["title"] as! String
-                    print(title)
-                }
+                self.movieList = movies
+                
+                self.tableView.reloadData()
             }
         }
         // call task
         task.resume()
+    }
+    
+    // table view data source protocol
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movieList[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
---
+++ FlixApp/FlixApp/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  FlixApp
+//
+//  Created by Xiaoyi Liu on 9/9/18.
+//  Copyright © 2018 Xiaoyi Liu. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
